d38fe6e890532b3f7f2619cffa3bca8d0441976b
sasa9995510 <111662844+sasa9995510@users.noreply.github.com>
2022-10-28 21:21:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -68,9 +68,9 @@ global.linkz = "https://chat.whatsapp.com/Lt0ebfpvw4z5alaBYh1GLm" //your theme u
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
+global.themeemoji = "💖" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "💖Dream Guy Xeon" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
